3ae2b2b5578bc7fcc169ff17e84b866fadc2476b
kashworth08 <ashworthkatelyn@cityuniversity.edu>
2025-06-07 22:03:04 +0000

    PE06 - Katelyn Ashworth

---
+++ CitiesApp/src/Countries/Country.js
@@ -0,0 +1,124 @@
+import React from 'react';
+import { View, Text, StyleSheet, ScrollView, TextInput, TouchableOpacity } from 'react-native';
+import CenterMessage from '../components/CenterMessage';
+import { colors } from '../theme';
+
+class Country extends React.Component {
+  state = {
+    name: '',
+    info: '',
+  };
+
+  onChangeText = (key, value) => {
+    this.setState({ [key]: value });
+  };
+
+  addCurrency = () => {
+    const { addCurrency, country } = this.props.route.params;
+    const { name, info } = this.state;
+
+    if (name === '' || info === '') return;
+
+    const currency = { name, info };
+
+    addCurrency(currency, country);
+
+    this.setState({ name: '', info: '' });
+  };
+
+  render() {
+    const { countries, country } = this.props.route.params;
+
+    // Derive the updated version of the country from the global countries array
+    const updatedCountry = countries.find((item) => item.id === country.id) || country;
+
+    return (
+      <View style={{ flex: 1 }}>
+        <ScrollView contentContainerStyle={[!updatedCountry.currencies.length && { flex: 1 }]}>
+          <View
+            style={[
+              styles.locationsContainer,
+              !updatedCountry.currencies.length && { flex: 1, justifyContent: 'center' },
+            ]}
+          >
+            {!updatedCountry.currencies.length && <CenterMessage message="No additional currencies for this country!" />}
+            {updatedCountry.currencies.map((currency, index) => (
+              <View key={index} style={styles.currencyContainer}>
+                <Text style={styles.currencyName}>{currency.name}</Text>
+                <Text style={styles.currencyUsed}>{currency.info}</Text>
+              </View>
+            ))}
+          </View>
+        </ScrollView>
+        <TextInput
+          onChangeText={(val) => this.onChangeText('name', val)}
+          placeholder="Currency name"
+          value={this.state.name}
+          style={styles.input}
+          placeholderTextColor="white"
+        />
+        <TextInput
+          onChangeText={(val) => this.onChangeText('info', val)}
+          placeholder="Currency Used (Y / N)"
+          value={this.state.info}
+          style={[styles.input, styles.input2]}
+          placeholderTextColor="white"
+        />
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity onPress={this.addCurrency}>
+            <View style={styles.button}>
+              <Text style={styles.buttonText}>Add Currency</Text>
+            </View>
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  CurrenciesContainer: {
+    paddingBottom: 104,
+  },
+  input: {
+    height: 50,
+    backgroundColor: colors.primary,
+    color: 'white',
+    paddingHorizontal: 8,
+    position: 'absolute',
+    width: '100%',
+    bottom: 104,
+    left: 0,
+  },
+  input2: {
+    bottom: 52,
+  },
+  buttonContainer: {
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    width: '100%',
+  },
+  button: {
+    height: 50,
+    backgroundColor: colors.primary,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: 'white',
+  },
+  currencyContainer: {
+    padding: 10,
+    borderBottomColor: colors.primary,
+    borderBottomWidth: 2,
+  },
+  currencyName: {
+    fontSize: 20,
+  },
+  currencyUsed: {
+    color: 'rgba(0, 0, 0, .5)',
+  },
+});
+
+export default Country;
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # CS624_PE06
+
+<h1>Input</h1>
+<p>The additional inputs on this assignment are the countries and additional currencies that the country may / may not use.</p>
+<h1>Process</h1>
+<p>The only update is adding a Stack that pulls the user's inputs on the Country.js page and displays it.</p>
+<h1>Output</h1>
+<p>The output is an app that shows any entered currencies and their information.</p>
